4c8c3adefbc3aa6a507af4e519ba20a8c627b5a1
Arun TR <aruntr369@gmail.com>
2022-04-30 00:40:35 +0530

    container done

--- lib/main.dart
+++ lib/main.dart
@@ -19,31 +19,98 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-final String name ="headding";
+  final String name = "headding";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white12,
+        backgroundColor: Colors.white,
         //appBar: AppBar(),
         body: SafeArea(
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: Column(children: [
-                Text(
-                name,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.red.shade300,
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold),
-              ),
-              TextButton(onPressed: (){
-
-              }, child: Text('Click Me'),
-              )
-              ],)
-            )));
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Container(
+                  color: Colors.yellow,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.red.shade300,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              print('Text Button Clicked');
+                            },
+                            child: Text('Click Me'),
+                          ),
+                          IconButton(onPressed: () {}, icon: Icon(Icons.mic))
+                        ],
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Elevated Button Clicked');
+                          },
+                          child: Text("Click Me2"))
+                    ],
+                  )),
+            ),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.green, width: 6),
+                  ),
+                  child: Center(child: Text('simple text')),
+                )),
+            Expanded(
+              child: Container(
+                  color: Colors.orange,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.red.shade300,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              print('Text Button Clicked');
+                            },
+                            child: Text('Click Me'),
+                          ),
+                          IconButton(onPressed: () {}, icon: Icon(Icons.mic))
+                        ],
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Elevated Button Clicked');
+                          },
+                          child: Text("Click Me2"))
+                    ],
+                  )),
+            )
+          ],
+        )));
   }
 }
